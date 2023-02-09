7db4245185dddc916dffc18edf98bb1548a8574f
Louis <louyoon89@gmail.com>
2023-02-08 20:33:15 -0800

    schema start

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,33 @@
 const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
 const dateFormat = require("../utils/date");
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: "Please let us know what you think",
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: ,
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+)
+
+
 
 const Thought = model("Thought", thoughtSchema);
 
