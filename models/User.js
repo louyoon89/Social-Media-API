74a26a2b296ee1d5665db019f69622c60c861445
Louis <louyoon89@gmail.com>
2023-02-08 19:55:06 -0800

    userSchema draft

--- models/User.js
+++ models/User.js
@@ -1,4 +1,39 @@
+const { Schema, model } = require("mongoose");
 
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: ,
+        },
+        thoughts: [
+            {
+                type: ,
+                ref: ,
+            }
+        ],
+        friends: [
+            {
+                type: ,
+                ref: "User",
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
 
+);
 
 module.exports = User;
