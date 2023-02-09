f6c7791adb6d0ecc2679cbcc1f7c531c7a20d31c
Louis <louyoon89@gmail.com>
2023-02-09 12:10:49 -0800

    router update, connection update (strictquery)

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -10,5 +10,16 @@ const {
 } = require("../../controllers/thoughtController");
 
 
+router.route("/").get(getThoughts).post(createThought);
+
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
 
 module.exports = router;
