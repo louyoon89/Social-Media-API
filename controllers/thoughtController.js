daba983b4ca8e42207be2ce45790f64794f7597a
Louis <louyoon89@gmail.com>
2023-02-09 10:08:04 -0800

    createthought, route start

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -26,5 +26,31 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
+
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((dbThoughtData) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { thoughts: dbThoughtData._id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res
+            .status(404)
+            .json({ message: "Thought created with no user id" });
+        }
+
+        res.json({ message: "Thought successfully created" });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 };
+
+
 module.exports = thoughtController;
