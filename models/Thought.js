2b6f630634b2b470358d4ba8023ed64b0f007f6c
Louis <louyoon89@gmail.com>
2023-02-08 20:36:07 -0800

    virtual

--- models/Thought.js
+++ models/Thought.js
@@ -21,13 +21,16 @@ const thoughtSchema = new Schema(
     },
     {
         toJSON: {
+            virtuals: true,
             getters: true,
         },
         id: false,
     }
-)
+);
 
-
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
 
 const Thought = model("Thought", thoughtSchema);
 
